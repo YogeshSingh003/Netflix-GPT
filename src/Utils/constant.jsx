ac7ef801d4ce44c9aa25261163b65fb8eef548d7
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2024-07-06 00:55:27 +0530

    API added

--- src/Utils/constant.jsx
+++ src/Utils/constant.jsx
@@ -1,2 +1,11 @@
 export const USER_AVATAR =
   "https://i.pinimg.com/474x/5b/50/e7/5b50e75d07c726d36f397f6359098f58.jpg";
+
+export const API_OPTION = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNDlkNDM4YjYxOGFhODhmMGFhNzcwNTcyOTBjODEwYSIsIm5iZiI6MTcyMDE5NDEwNC4wNDQ1MDEsInN1YiI6IjY2ODgxMzA5N2JhOGNlMzI4ZDFiODViOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.inWEg1691jHDCnBwpU-E8JIs7Hove3wtU5tZeiFa0a0",
+  },
+};
