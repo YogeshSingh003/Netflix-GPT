bfe8405bd2fc7607dfd0f4a14edc975abfa081fc
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2024-07-07 16:32:03 +0530

    Custom hook useNowPlayingMovies

--- src/component/Browse.jsx
+++ src/component/Browse.jsx
@@ -1,48 +1,11 @@
-import { useEffect, useState } from "react";
-import { API_OPTION } from "../Utils/constant";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import Header from "./Header";
 
 const Browse = () => {
-  const [page, setPage] = useState(1);
-
-  const pageIncrement = () => {
-    setPage(page + 1);
-    console.log("https://api.themoviedb.org/3/movie/now_playing?page=" + page);
-  };
-
-  const pageDecrement = () => {
-    setPage(page - 1);
-    console.log("https://api.themoviedb.org/3/movie/now_playing?page=" + page);
-  };
-
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=" + page,
-      API_OPTION
-    );
-    const json = await data.json();
-    console.log(json);
-  };
-
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, [page]);
-
+  useNowPlayingMovies();
   return (
     <div>
       <Header />
-      <div className="flex flex-col items-center w-full justify-center h-screen">
-        <h2>{page}</h2>
-        <div>
-          <button
-            className="border-red-700 border-[3px]"
-            onClick={pageIncrement}
-          >
-            +
-          </button>
-          <button onClick={pageDecrement}>-</button>
-        </div>
-      </div>
     </div>
   );
 };
---
+++ src/hooks/useNowPlayingMovies.jsx
@@ -0,0 +1,23 @@
+import { useDispatch } from "react-redux";
+import { API_OPTION } from "../Utils/constant";
+import { addNowPlayingMovies } from "../Utils/moviesSlice";
+import { useEffect } from "react";
+
+const useNowPlayingMovies = () => {
+  const dispatch = useDispatch();
+  const getNowPlayingMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?page=1",
+      API_OPTION
+    );
+    const json = await data.json();
+    console.log(json.results);
+    dispatch(addNowPlayingMovies(json.results));
+  };
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+};
+
+export default useNowPlayingMovies;
