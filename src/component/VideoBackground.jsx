6a7ec72beb94e3855af1997f7bdf6a28013be15a
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2024-07-08 00:59:44 +0530

    retrive movie and trailer ID

--- src/component/VideoBackground.jsx
+++ src/component/VideoBackground.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { useEffect } from "react";
+import { API_OPTION } from "../Utils/constant";
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailerVideo } from "../Utils/moviesSlice";
 
-const VideoBackground = () => {
-  return <div>VideoBackground</div>;
+const VideoBackground = ({ movieId }) => {
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+  const dispatch = useDispatch();
+
+  const getMovieVideos = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/" +
+        movieId +
+        "/videos?language=en-US",
+      API_OPTION
+    );
+    const json = await data.json();
+    console.log(json);
+
+    const filterData = json.results.filter((video) => video.type === "Trailer");
+    const trailer = filterData.length ? filterData[0] : json.results[0];
+    console.log(filterData);
+    console.log(trailer);
+    dispatch(addTrailerVideo(trailer));
+  };
+
+  console.log(trailerVideo.key);
+  useEffect(() => {
+    getMovieVideos();
+  }, []);
+  return (
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </div>
+  );
 };
 
 export default VideoBackground;
