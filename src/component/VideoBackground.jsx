c0b6a2c84afd27c075f35462a962d4b69741bee4
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2024-07-08 21:17:14 +0530

    versal.json

--- src/component/VideoBackground.jsx
+++ src/component/VideoBackground.jsx
@@ -35,13 +35,13 @@ const VideoBackground = ({ movieId }) => {
   }, []);
   return (
     <div>
-      {/* <iframe
+      <iframe
         width="560"
         height="315"
         src={"https://www.youtube.com/embed/" + trailerVideo?.key}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      ></iframe> */}
+      ></iframe>
     </div>
   );
 };
