1dad3ded8785b6e89934f6c550175445a181d096
Yogesh Singh <84557278+YogeshSingh003@users.noreply.github.com>
2024-07-10 14:23:11 +0000

    vscode.dev testing

--- src/component/SecondaryContainer.jsx
+++ src/component/SecondaryContainer.jsx
@@ -1,7 +1,9 @@
-import React from "react";
-
 const SecondaryContainer = () => {
-  return <div>SecondaryContainer</div>;
+  return (
+    <div>
+      <h1 className="text-blue-700">SecondaryContainer</h1>
+    </div>
+  );
 };
 
 export default SecondaryContainer;
