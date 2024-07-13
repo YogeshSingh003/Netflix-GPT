67e5eed8c8c0f60e382f3924f4717d9144c21698
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2024-07-13 14:00:00 +0530

    revert

--- src/Utils/firebase.jsx
+++ src/Utils/firebase.jsx
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC1ygDGlTUfdhs86fhg0d-lIW3qxjbX-Lo",
+  apiKey: import.meta.env.VITE_REACT_API_KEY,
   authDomain: import.meta.env.VITE_REACT_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_REACT_PROJECT_ID,
   storageBucket: import.meta.env.VITE_REACT_STORAGE_BUCKET,
