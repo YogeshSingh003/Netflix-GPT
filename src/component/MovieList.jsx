f9145c6ba4286de1be3ede6e28113b2a14e0031d
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2024-07-13 16:26:40 +0530

    BugFix: CSS of movie list

--- src/component/MovieList.jsx
+++ src/component/MovieList.jsx
@@ -4,7 +4,7 @@ const MovieList = ({ title, movies }) => {
   return (
     <div className="overflow-x-hidden px-6  text-white ">
       <h1 className="text-3xl font-bold py-6">{title}</h1>
-      <div className="flex overflow-x-scroll scrollbar-none">
+      <div className="flex overflow-x-scroll overflow-y-hidden scrollbar-none">
         <div className="flex">
           {movies?.map((movie) => (
             <MovieCard key={movie.id} posterPath={movie.poster_path} />
