c89520c4fbcdc6305d4510b9332796f5b5555567
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2024-09-12 23:32:24 +0530

    Movie results added in redux slice

--- src/Utils/gptSlice.jsx
+++ src/Utils/gptSlice.jsx
@@ -1,16 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const gptSlice = createSlice({
-    name: "gpt",
-    initialState: {
-        showGptSearch: false
+  name: "gpt",
+  initialState: {
+    showGptSearch: false,
+    movieNames: null,
+    movieList: null,
+  },
+  reducers: {
+    toggleGptSearchView: (state) => {
+      state.showGptSearch = !state.showGptSearch;
     },
-    reducers: {
-        toggleGptSearchView: (state) => {
-            state.showGptSearch = !state.showGptSearch;
-        }
-    }
+    addGptMovieResults: (state, action) => {
+      const { movieNames, movieList } = action.payload;
+      state.movieNames = movieNames;
+      state.movieList = movieList;
+    },
+  },
 });
 
-export const {toggleGptSearchView} = gptSlice.actions;
+export const { toggleGptSearchView, addGptMovieResults } = gptSlice.actions;
 export default gptSlice.reducer;
